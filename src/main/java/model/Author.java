68f5d9019ef18f14943768573cefca780b5f2135
Андрей Крупников <cjrrf@mail.ru>
2023-01-14 23:11:05 +0400

    task4 complete

--- src/main/java/model/Author.java
+++ src/main/java/model/Author.java
@@ -2,18 +2,38 @@ package model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
 import java.util.List;
 import java.util.UUID;
 
 @Getter
 @Setter
-public class Author {
+public class Author implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID")
     private UUID id;
+
+    @NotNull
+    @Column(name = "NAME")
     private String name;
+
+    @NotNull
+    @Column(name = "SURNAME")
     private String surname;
+
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @OneToMany(mappedBy = "author")
     private List<Book> bookList;
+
+    @NotNull
+    @Column(name = "COUNTRY")
     private String country;
 
 }
